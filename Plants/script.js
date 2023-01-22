a9a1fbb59487c996508ed7f84b48a63d4ba035b3
apribooting <roflanPushka@gmail.com>
2023-01-22 16:21:57 +0300

    feat: add console.log

--- Plants/script.js
+++ Plants/script.js
@@ -1,3 +1,4 @@
+console.log("1. 24/24 \n 2. 24/24 \n 3. 15/15 \n 4. Нету крестика, меню не скрывается при нажатии на ссылки, скрывается плавно, 16/22 \n Total 79/85 ");
 (function () {
     const burgerItem = document.querySelector('.burger');
     const burgerMenu = document.querySelector('.menu');
@@ -13,3 +14,4 @@
        burgerMenu.classList.toggle("burger_menu_active");  
     });
 }()); 
+
