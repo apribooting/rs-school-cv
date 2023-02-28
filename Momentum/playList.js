fb1e3c945d2417091b75d3edbbc038ab0f03c6f9
apribooting <roflanPushka@gmail.com>
2023-02-28 22:54:04 +0300

    feat: add player

---
+++ Momentum/playList.js
@@ -0,0 +1,19 @@
+const playList = [
+    {
+        title: 'Aqua',
+        src: './assets/audio/aqua.mp3',
+        duration: '00:39'
+    },
+
+    {
+        title: 'River',
+        src:'./assets/audio/River.mp3',
+        duration: '01:37'
+    },
+    {
+        title: 'Wind',
+        src: './assets/audio/Wind.mp3',
+        duration: '01:50'
+    }
+]
+export default playList;
--- Momentum/script.js
+++ Momentum/script.js
@@ -1,15 +1,75 @@
+import playList from './playList.js';
+
 const player = document.querySelector('.player'),
       playBnt = document.querySelector('.play'),
       prewBnt = document.querySelector('.play-prev'),
       nextBnt = document.querySelector('.play-next'),
       audio = document.querySelector('.audio'),
-      playItem = document.querySelector('.play-item'),
-      pause = document.querySelector('.pause')
-
-    const songs = ['Aqua', 'Wind','River']
-(function () {
+      playItem = document.querySelectorAll('.play-item'),
+      pause = document.querySelector('.pause');
+      let song= (playList.length - 3);
+      
+        audio.src = playList[song].src;
+    (function (){
+       
     playBnt.addEventListener("click", () => {
+
         playBnt.classList.toggle("pause");
+        playItem[0].classList.remove('color_red');
+        playItem[1].classList.remove('color_red');
+        playItem[2].classList.remove('color_red');
+        playItem[song].classList.toggle('color_red');
+        
+        audio.play();
+        if (playBnt.classList.contains('pause')===false) {
+            audio.pause()}
+            
     });
-}())
-   
+    console.log(playList);
+    console.log(song);
+
+    prewBnt.addEventListener("click", () => {
+        song -=1;
+        if (song < 0) {
+            song = 2
+        }
+        playItem[0].classList.remove('color_red');
+        playItem[1].classList.remove('color_red');
+        playItem[2].classList.remove('color_red');
+        playItem[song].classList.toggle('color_red');
+        audio.src = playList[song].src;
+        if (playBnt.classList.contains('pause')===true) {
+            console.log("7");
+            audio.play()}
+            
+    });
+
+    nextBnt.addEventListener("click", () => {
+        song +=1;
+        if (song === 3) {
+            song = 0
+        }
+        audio.src = playList[(song)].src;
+        playItem[0].classList.remove('color_red');
+        playItem[1].classList.remove('color_red');
+        playItem[2].classList.remove('color_red');
+        playItem[song].classList.toggle('color_red');
+        if (playBnt.classList.contains('pause')===true) {
+            console.log("1");
+            audio.play()}
+    });
+    audio.onended = function(){
+        console.log('5');
+        song ++;
+        if (song === 3) {
+            song = 0}
+        this.src = playList[(song)].src;
+        this.play();
+    }
+    
+}()); 
+
+
+
+
+
