8eb0f577a09c80a2334018992a41d622f2cba1d3
apribooting <roflanPushka@gmail.com>
2023-03-01 17:00:08 +0300

    feat: add clock

--- Momentum/script.js
+++ Momentum/script.js
@@ -69,7 +69,57 @@ const player = document.querySelector('.player'),
     
 }()); 
 
+function showDate() {
+    
+    const month = document.querySelector('.date')
+    const a = new Date();
+    const options = {month: 'long', day: 'numeric'};
+    const currentDate = a.toLocaleDateString('en-En', options);
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = (days[a.getDay()])
+    
+    month.textContent = (day+ ", " + currentDate); 
+}
 
+function showTime() {
+const time = document.querySelector('.time');
+const date = new Date();
+const currentTime = date.toLocaleTimeString();
+setTimeout(showTime, 1000);
+time.textContent = currentTime;
+showDate() 
+}
+showTime();
 
+function showGreeting () {
+    const greeting = document.querySelector(".greeting");
+    var welcome;
+    const myDate = new Date();
+    const hour = myDate.getHours()
+    if (hour < 12) {
+        welcome = "Good morning";
+    } else if (hour < 17) {
+        welcome = "Good afternoon";
+    } else if (hour < 22 ){
+        welcome = "Good evening";
+    }   else {
+        welcome = "Good night";
+    }
+    
+    greeting.textContent = welcome;
+}
 
+showGreeting();
 
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+  
+  function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+  }
+  window.addEventListener('load', getLocalStorage)
