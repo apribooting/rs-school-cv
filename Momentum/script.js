b604786a75d0cf30e1223367c256ece9a2701e60
apribooting <roflanPushka@gmail.com>
2023-03-02 16:25:57 +0300

    feat: add weather, todo, quote

--- Momentum/script.js
+++ Momentum/script.js
@@ -116,17 +116,6 @@ function showGreeting () {
 
 showGreeting();
 
-function setLocalStorage() {
-    localStorage.setItem('name', name.value);
-  }
-  window.addEventListener('beforeunload', setLocalStorage)
-  
-  function getLocalStorage() {
-    if(localStorage.getItem('name')) {
-      name.value = localStorage.getItem('name');
-    }
-  }
-  window.addEventListener('load', getLocalStorage)
 
 
 
@@ -175,12 +164,13 @@ function setLocalStorage() {
       
     const abc = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"
     const abb = (abc + dayPart + "/" + lastRandom + ".jpg')");
+    
     document.body.style.backgroundImage = abb;
     const prewBg = document.querySelector(".slide-prew");
     const nextBg = document.querySelector(".slide-next");
-     
+    
      nextBg.addEventListener("click", () => {
-        const prevRandom = (Math.random() * 20);
+        const prevRandom = (Math.random() * 20 );
    
         const random = Math.ceil(prevRandom);
         
@@ -207,8 +197,12 @@ function setLocalStorage() {
             }
           
         const abc = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"
+        const preImg = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/'
         const abb = (abc + dayPart + "/" + lastRandom + ".jpg')");
-        document.body.style.backgroundImage = abb;
+        const img = new Image();
+        img.src = (preImg + dayPart + "/" + lastRandom + ".jpg");
+        img.onload = () => {
+        document.body.style.backgroundImage = abb};
     });
     prewBg.addEventListener("click", () => {
         const prevRandom = (Math.random() * 20);
@@ -238,18 +232,290 @@ function setLocalStorage() {
             }
           
         const abc = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/"
+        const preImg = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/'
         const abb = (abc + dayPart + "/" + lastRandom + ".jpg')");
-        document.body.style.backgroundImage = abb;
+        const img = new Image();
+        img.src = (preImg + dayPart + "/" + lastRandom + ".jpg");
+        img.onload = () => {
+        document.body.style.backgroundImage = abb};
     });
     
 }
 
 setBg()
 
-async function getQuotes() {  
-    const quotes = 'data.json';
+function setLocalStorage() {
+    const name = document.querySelector(".name");
+    localStorage.setItem('name', name.value);
+    }
+    window.addEventListener('beforeunload', setLocalStorage);
+    
+    function getLocalStorage() {
+    const name =document.querySelector(".name")
+    if(localStorage.getItem('name')) {
+        name.value = localStorage.getItem('name');
+    }
+    }
+    window.addEventListener('load', getLocalStorage);
+let lang = "en";
+    const city =document.querySelector(".city");
+    const weatherIcon = document.querySelector('.weather-icon');
+    const temperature = document.querySelector('.temperature');
+    const weatherDescription = document.querySelector('.weather-description');
+    const wind = document.querySelector('.wind');
+    const humidity = document.querySelector('.humidity');
+    async function getWeather(a) {
+        const urlTranlation ={
+            'en':[[`https://api.openweathermap.org/data/2.5/weather?q=${a}&lang=en&appid=b332b3c13a9eb7954f5fa649326d9241&units=metric`],['Wind speed'],['Humidity']],
+            'ru':[[`https://api.openweathermap.org/data/2.5/weather?q=${a}&lang=ru&appid=b332b3c13a9eb7954f5fa649326d9241&units=metric`],['Скорость ветра'],['Влажность']]
+        }
+        const url = urlTranlation[lang][0][0];
+        const res = await fetch(url);
+        const data = await res.json();
+        const weatherError = document.querySelector('.weather-error');
+        if (data.cod==='404'){
+            weatherIcon.className = 'weather-icon owf';
+            weatherError.textContent='city not found';
+            temperature.textContent ='';
+            weatherDescription.textContent = '';
+            wind.textContent = '';
+            humidity.textContent = '';
+        }else {
+            weatherError.textContent='';
+            weatherIcon.className = 'weather-icon owf';
+            weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+            temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+            weatherDescription.textContent = data.weather[0].description;
+            wind.textContent = `${urlTranlation[lang][1][0]}: ${Math.floor(data.wind.speed)} m/s`;
+            humidity.textContent = `${urlTranlation[lang][2][0]}: ${data.main.humidity} %`;
+        }
+    }
+    const cityTranslation = {
+        'en':'Minsk',
+        'ru':'Минск'
+    }
+    city.value =  cityTranslation[lang]
+    getWeather(city.value);
+    localStorage.setItem('city', city.value);
+    
+    function cityValue(){
+        city.value = cityTranslation[lang];
+    }
+cityValue()
+    function setLocalStorageCity() {
+        localStorage.setItem('city', city.value);
+        getWeather(city.value);
+    }
+    city.addEventListener('change', setLocalStorageCity);
+    
+    function getLocalStorageCity() {
+        if(localStorage.getItem('city')) {
+            city.value = localStorage.getItem('city');
+        }
+        getWeather(city.value);
+    }
+    window.addEventListener('load', getLocalStorageCity);
+function getRandom(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+    }   
+
+const quote = document.querySelector('.quote');
+const author =document.querySelector('.author');
+const changeQuote = document.querySelector('.change-quote')
+async function getQuotes() {
+    const quotesTranslation= {
+        'en':'./assets/json/frazy.json',
+        'ru':'./assets/json/frazy.json'
+    }
+    const quotes = quotesTranslation[lang];
     const res = await fetch(quotes);
-    const data = await res.json(); 
-    console.log(data);
-  }
-  getQuotes();
+    const data = await res.json();
+    const numberQuote = getRandom(0,(data.length-1));
+    quote.textContent = `\"${data[numberQuote].text}\"`;
+    author.textContent = data[numberQuote].author;
+}
+getQuotes();
+changeQuote.addEventListener('click',()=>{
+    getQuotes();
+})
+
+const languageTodolist = {
+    'en':['New taks','Add','Resting','Todo list'],
+    'ru':['Новое задание','Добавить','Список дел пуст','Список дел']
+}
+const titleTodolist = document.querySelector('.todolist-title');
+const todolistInput = document.querySelector('.todolist-form-input');
+const todolistButton = document.querySelector('.todolist-form-button');
+const listNoneText = document.querySelector('.list-none-text');
+const todolistForm = document.querySelector('.todolist-form');
+const listAvailable = document.querySelector('.list-available');
+function translationTodolist (){
+    todolistInput.placeholder = languageTodolist[lang][0];
+    todolistButton.textContent = languageTodolist[lang][1];
+    listNoneText.textContent = languageTodolist[lang][2];
+    titleTodolist.textContent =languageTodolist[lang][3];
+}
+translationTodolist();
+let numberListElement = 1;
+function addTodolist(event){
+    event.preventDefault();
+    document.querySelector('.list-none').classList.add('none');
+    listAvailable.classList.add('list-available-open');
+    let divTodolist = document.createElement('div');
+    divTodolist.classList.add('todo-list-element');
+    let inputTodolist = document.createElement("input");
+    inputTodolist.classList.add('input-todolist');
+    let label1 = document.createElement('label');
+    inputTodolist.id = `${numberListElement}`;
+    inputTodolist.type = "checkbox";  
+    label1.setAttribute("for", `${numberListElement}`);
+    label1.classList.add('todo-list-element-label')
+    label1.innerHTML = todolistInput.value;
+    let spanClose = document.createElement("span");
+    spanClose.classList.add('todo-list-element-span');
+    spanClose.textContent ="X";    
+    divTodolist.appendChild(inputTodolist);
+    divTodolist.appendChild(label1);
+    divTodolist.appendChild(spanClose);
+    listAvailable.appendChild(divTodolist);
+    todolistInput.value = "";
+    numberListElement += 1;
+    inputTodolist = document.querySelectorAll('.input-todolist');
+    inputTodolist.forEach(el=>{
+        el.addEventListener('click', clickTodo);
+    });
+    divTodolist = document.querySelectorAll('.todo-list-element')
+    divTodolist.forEach(item=>{
+        item.addEventListener('mouseover',hoverTodo);
+        item.addEventListener('mouseout',hoverTodo);
+    });
+    
+}
+function addTodolistLocalStorage(el){
+    document.querySelector('.list-none').classList.add('none');
+    listAvailable.classList.add('list-available-open');
+    let divTodolist = document.createElement('div');
+    divTodolist.classList.add('todo-list-element');
+    let inputTodolist = document.createElement("input");
+    inputTodolist.classList.add('input-todolist');
+    let label1 = document.createElement('label');
+    inputTodolist.id = `${numberListElement}`;
+    inputTodolist.type = "checkbox";  
+    label1.setAttribute("for", `${numberListElement}`);
+    label1.classList.add('todo-list-element-label')
+    label1.innerHTML = el;
+    let spanClose = document.createElement("span");
+    spanClose.classList.add('todo-list-element-span');
+    spanClose.textContent ="X";    
+    divTodolist.appendChild(inputTodolist);
+    divTodolist.appendChild(label1);
+    divTodolist.appendChild(spanClose);
+    listAvailable.appendChild(divTodolist);
+    numberListElement += 1;
+    inputTodolist = document.querySelectorAll('.input-todolist');
+    inputTodolist.forEach(el=>{
+        el.addEventListener('click', clickTodo);
+    });
+    divTodolist = document.querySelectorAll('.todo-list-element')
+    divTodolist.forEach(item=>{
+        item.addEventListener('mouseover',hoverTodo);
+        item.addEventListener('mouseout',hoverTodo);
+    });
+    
+}
+todolistForm.addEventListener('submit', addTodolist);
+function removetodo(event){
+    event.currentTarget.parentNode.remove();
+    let divTodolist = document.querySelectorAll('.todo-list-element');
+    console.log(divTodolist)
+    if(divTodolist.length===0){
+        document.querySelector('.list-none').classList.remove('none');
+        listAvailable.classList.remove('list-available-open');
+    }
+
+}
+function clickTodo(event){
+    event.currentTarget.nextElementSibling.classList.toggle('input-todolist-inactive');
+}
+function clickTodoLocalStorage(el){
+    let labelTodo = document.querySelectorAll('.todo-list-element-label');
+    labelTodo.forEach(item=>{
+        if(item.textContent===el){
+            item.classList.toggle('input-todolist-inactive');
+            item.previousSibling.checked=true;
+        }
+    })
+    
+}
+function hoverTodo(event){
+    event.currentTarget.lastChild.classList.toggle('todo-list-element-span-hover');
+    event.currentTarget.lastChild.addEventListener('click',removetodo)
+}
+
+
+function returnallTodoList (Nameclass){
+    let allTodolist ='';
+    document.querySelectorAll(`.${Nameclass}`).forEach(el=>{
+        allTodolist += `${el.textContent} `;
+    });
+    return allTodolist;
+}
+function setLocalStorageSetting() {    
+    localStorage.setItem('language', lang);
+    localStorage.setItem('photoAPI', photoApi);  
+    localStorage.setItem('timeOfDayUnslash', tagUnsplash.value);
+    localStorage.setItem('timeOfDayflickr', tagflickr.value);
+   
+    let allTodolist = returnallTodoList('todo-list-element-label');
+    localStorage.setItem('allTodolist', allTodolist);
+    let allTodolistinactive = returnallTodoList('input-todolist-inactive');
+    localStorage.setItem('allTodolistinactive', allTodolistinactive);
+}
+window.addEventListener('beforeunload', setLocalStorageSetting);
+const checkbox = document.querySelectorAll('.visibility-checkbox');
+
+function getLocalStorageSetting() {
+    if(localStorage.getItem('language')) {
+        lang = localStorage.getItem('language');        
+    }    
+    getLocalStorageCity();
+    cityValue();
+    getQuotes();    
+
+    translationTodolist();
+   
+    
+    if (localStorage.getItem('photoAPI')==='flickr'){
+        timeOfDayflickr = tagflickr.value;
+        clickFlickrButton();
+        clickburger();
+    }else if(localStorage.getItem('photoAPI')==='unsplash'){
+        timeOfDayUnslash = tagUnsplash.value;
+        clickUnsplashButton();
+        clickburger();
+    }
+    checkbox.forEach((el,ind)=>{        
+        if(localStorage.getItem(`${el.id}`)==='false'){
+            el.checked = false;
+            changeCheckbox(el);
+        }else{
+            el.checked = true;
+        }
+    })
+    let allTodolist = localStorage.getItem('allTodolist');
+    allTodolist = allTodolist.slice(0,(allTodolist.length-1));
+    allTodolist.forEach(el=>{
+    addTodolistLocalStorage(el);
+    })
+    let allTodolistinactive= localStorage.getItem('allTodolistinactive').split(' ');
+    allTodolistinactive = allTodolistinactive.slice(0,(allTodolistinactive.length-1));
+    allTodolistinactive.forEach(el=>{
+        clickTodoLocalStorage(el);
+    })   
+    
+
+    
+}
+window.addEventListener('load', getLocalStorageSetting);
